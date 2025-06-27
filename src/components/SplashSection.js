389b4fdca651029e0c29f9d179b1ae37d13da967
ashickarcon <ashick.antony@arcon.biz>
2025-06-27 17:33:12 +0200

    carosal final

--- src/components/SplashSection.js
+++ src/components/SplashSection.js
@@ -12,7 +12,7 @@ const slides = [
   {
     title: "Fan Fest 2025 - Where travel meets football",
     subtitle: "Register now and get 10% off your next AfineTrip flight",
-    background: bg2,
+    background: bg1,
     textStyle: {
   color: "#d88371",
   textShadow: "1px 1px 2px rgba(255,255,255,0.3)",
@@ -51,40 +51,37 @@ const SplashSection = () => {
   };
 
   return (
-    <section
-      className="splash-section-bootstrap"
-      style={{
-        backgroundImage: `url(${slides[index].background})`,
-        // backgroundSize: "cover",
-        // backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        transition: "background-image 0.5s ease-in-out", // smooth fade transition of bg
-        color: "white",
-      }}
-    >
-      <Carousel
-        activeIndex={index}
-        onSelect={handleSelect}
-        interval={5000}
-        controls={false}
-        indicators={false}
-      >
-       {slides.map((slide, idx) => (
-  <Carousel.Item key={idx}>
-    <div className="carousel-content-bootstrap">
-      <div className="carousel-animation-bootstrap">
-        <Lottie animationData={soccerball2} loop autoplay />
-      </div>
-      <div className="carousel-text-bootstrap" style={slide.textStyle}>
-        <h2>{slide.title}</h2>
-        <p>{slide.subtitle}</p>
-      </div>
-    </div>
-  </Carousel.Item>
-))}
+<section
+  className="splash-section-bootstrap small-carousel"
+  style={{
+    backgroundImage: `url(${slides[index].background})`,
+    backgroundRepeat: "no-repeat",
+    transition: "background-image 0.5s ease-in-out",
+  }}
+>
+  <Carousel
+    activeIndex={index}
+    onSelect={handleSelect}
+    interval={5000}
+    controls={false}
+    indicators={false}
+  >
+    {slides.map((slide, idx) => (
+      <Carousel.Item key={idx}>
+        <div className="carousel-content-bootstrap">
+          <div className="carousel-animation-bootstrap">
+            <Lottie animationData={soccerball2} loop autoplay />
+          </div>
+          <div className="carousel-text-bootstrap" style={slide.textStyle}>
+            <h2>{slide.title}</h2>
+            <p>{slide.subtitle}</p>
+          </div>
+        </div>
+      </Carousel.Item>
+    ))}
+  </Carousel>
+</section>
 
-      </Carousel>
-    </section>
   );
 };
 
